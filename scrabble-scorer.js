0323888a7d887a7dcb38237da14338f18e7ac2c0
GraceDebHopper <ebbienextmove@gmail.com>
2021-02-27 17:20:01 +0000

    Assign.#2 done thru C(Transform)

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,6 +1,8 @@
 // inspired by https://exercism.io/tracks/javascript/exercises/etl/solutions/91f99a3cca9548cebe5975d7ebca6a85
 
 const input = require("readline-sync");
+let word = '';            //works with or without for A) and B)
+let scoreMethod = 0;      //works with or without for B)
 
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
@@ -33,26 +35,118 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+  console.log("Let's play some Scrabble!");
+  console.log();
+  word = input.question('Enter a word to score: ');
+  return word;
 };
 
-let simpleScore;
+let simpleScore = {
+  name: "Simple Score",
+  description: "Each letter is worth 1 point",
+  scorerFunction: function simpleScorer(word) {
+    let simpleScore = 0;
+    for(let i=0; i<word.length; i++) {
+      simpleScore ++;
+    }
+    return simpleScore;
+  }
+};
+//console.log(simpleScore);
+//console.log('0 ' + simpleScore.scorerFunction(word));
 
-let vowelBonusScore;
+let vowelBonusScore = {
+  name: "Bonus Vowels",
+  description: "Vowels are 3 pts, consonaants are 1 pt.",
+  scorerFunction: function vowelBonusScorer(word) {
+    word = word.toLowerCase();
+    let vowelBonusScorer = 0;
+    for(let i=0; i<word.length; i++) {
+      if(word[i].includes('a') || word[i].includes('e') || word[i].includes('i') || word[i].includes('o') || word[i].includes('u')){
+        vowelBonusScorer += 3;
+      } else {
+        vowelBonusScorer ++;
+      }
+    }
+    return vowelBonusScorer;            //typeof Number
+  }
+};
+//console.log(vowelBonusScore);
+//console.log('1 ' + vowelBonusScore.scorerFunction(word));
 
-let scrabbleScore;
+let scrabbleScore = {
+  name: "Scrabble",
+  description: "The traditional scoring algorithm.",
+  scorerFunction: function scrabbleScorer(word) {
+    word = word.toLowerCase();
+    let scrabbleScore = 0;
+    for(let i = 0; i < word.length; i++) {
+      let letter = word.charAt(i);
+      scrabbleScore += Number(newPointStructure[letter]);
+    }
+    return scrabbleScore;
+  }
+};
 
-const scoringAlgorithms = [];
+/*let scrabbleScore = {
+  name: "Scrabble",
+  description: "The traditional scoring algorithm.",
+  scorerFunction: function oldScrabbleScorer(word) {
+    word = word.toUpperCase();
+    let letterPoints = "";
+    for(let i = 0; i < word.length; i++) {
+      for (const pointValue in oldPointStructure) {
+        if (oldPointStructure[pointValue].includes(word[i])){
+          letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+        }
+      }
+    }
+    return letterPoints;
+  }
+};*/
+//console.log(scrabbleScore);
+//console.log('2 ' + scrabbleScore.scorerFunction(word));
 
-function scorerPrompt() {}
 
-function transform() {};
+const scoringAlgorithms = [simpleScore,vowelBonusScore,scrabbleScore];
+//console.log(scoringAlgorithms);
 
-let newPointStructure;
+//input.question returns str and array indexes are num - convert
+//variables (word and scoreMethod) used o/s function need to be declared/defined o/s function or ReferenceError: x is not defined
+function scorerPrompt() {
+  console.log(`Which scoring algorithm would you like to use?
+  
+0 - Simple: One point per character
+1 - Vowel Bonus: Vowels are worth 3 points
+2 - Scrabble: Uses scrabble point system`);
+  scoreMethod = Number(input.question('Enter 0 ,1, or 2: '));
+  //console.log(typeof scoreMethod);
+  return(scoringAlgorithms[scoreMethod]);
+}
+
+let newPointStructure = transform(oldPointStructure);
+
+function transform(oldPointStructure) {
+  let newPointStructure = {};
+  for(pointValue in oldPointStructure){
+    for(let i=0; i<oldPointStructure[pointValue].length; i++){
+      letter = oldPointStructure[pointValue][i];
+      newPointStructure[letter.toLowerCase()] = Number(pointValue);
+    }
+  }
+return newPointStructure;
+}
+//console.log(oldPointStructure);
+//console.log(newPointStructure);
+
+//let newPointStructure = transform(oldPointStructure);
+//original here after transform func w/ error so moved to prior 
 
 function runProgram() {
    initialPrompt();
-   
+   //console.log(oldScrabbleScorer(word));
+   scorerPrompt();
+   console.log(`Score for '${(word)}': ${scoringAlgorithms[scoreMethod].scorerFunction(word)}`);   
 }
 
 // Don't write any code below this line //
